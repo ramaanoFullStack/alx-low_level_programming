57cd9a48ff6b01fc2940ada827778d6cc185da3a
ramaanoFullStack <ramaano.m@gmail.com>
2022-09-08 12:13:16 -0700

    C program that prints exactly [statement given]

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#inlcude <unistd.h>
+
+/**
+* main - start program
+*
+* Return: is 1 (Success)
+*/
+int main(void)
+{
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n\", 59);
+return (1);
+}
