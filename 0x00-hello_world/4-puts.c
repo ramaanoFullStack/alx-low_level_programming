1dc9dd5a42302367a06e5e69b25ae3c66d338113
ramaanoFullStack <ramaano.m@gmail.com>
2022-09-08 08:40:44 -0700

    Added betty styled comments and removed empty spaces at the beggining of a line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+* main is the entry point and we use the function puts to print
+*
+* Program ends with the value 0 (Success)
+*/
+
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
