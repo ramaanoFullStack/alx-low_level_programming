efc3ef9ed990278e795bcbda26f26b6d75c64a82
ramaanoFullStack <ramaano.m@gmail.com>
2022-09-08 08:58:04 -0700

    C program that prints exactly [Statement given]

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - start program
+*
+* Return: is 0 (Success)
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return(0);
+}
