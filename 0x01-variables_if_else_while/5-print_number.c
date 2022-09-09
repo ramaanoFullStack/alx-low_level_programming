0bd278ddf23cdddef793deb15e5a7cf0857365c9
ramaanoFullStack <ramaano.m@gmail.com>
2022-09-09 08:29:06 -0700

    Program that prints all single digit numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - start program
+*
+* Return: is 0 (Success)
+*/
+int main(void)
+{
+int rsn;
+
+for (rsn = 0; rsn < 10; rsn++)
+printf("%d", rsn);
+printf("\n");
+return (0);
+}
