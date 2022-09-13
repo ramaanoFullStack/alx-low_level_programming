406d9668ca42ee7d9602a1c657d3d4af269724b1
ramaanoFullStack <ramaano.m@gmail.com>
2022-09-13 12:05:57 -0700

    returns the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _abs - start program
+*
+* @n: interger n
+* Return: int
+*/
+int _abs(int n)
+{
+if (n > 0)
+{
+return (n);
+}
+else if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
+}
