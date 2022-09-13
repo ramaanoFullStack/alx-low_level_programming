8cef1a6d518385f328223d9fb44fbf3b80c0d38e
ramaanoFullStack <ramaano.m@gmail.com>
2022-09-13 14:05:17 -0700

    Prints the 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,35 +7,35 @@
 */
 void times_table(void)
 {
-  int row, column, multiply, double, single;
+int row, column, multiply, rsdouble, single;
 
-  for (row = 0; row <= 9; row++)
-    {
-      for (column = 0; column <= 9; column++)
-	{
-	  multiply = row * column;
-	  double = multiply / 10;
-	  single = multiply % 10;
+for (row = 0; row <= 9; row++)
+{
+for (column = 0; column <= 9; column++)
+{
+multiply = row * column;
+rsdouble = multiply / 10;
+single = multiply % 10;
 
-	  if (column == 0)
-	    {
-	      _putchar('0');
-	    }
-	  else if (multiply < 10)
-	    {
-	      _putchar(',');
-	      _putchar(' ');
-	      _putchar(' ');
-	      _putchar(ones + '0');
-	    }
-	  else
-	    {
-	      _putchar(',');
-	      _putchar(' ');
-	      _putchar(double + '0');
-	      _putchar(single + '0');
-	    }
-	}
-      _putchar('\n');
-    }
+if (column == 0)
+{
+_putchar('0');
+}
+else if (multiply < 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(single + '0');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+_putchar(rsdouble + '0');
+_putchar(single + '0');
+}
+}
+_putchar('\n');
+}
 }
