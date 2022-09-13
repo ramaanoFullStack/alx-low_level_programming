7a47653df70459549010134fd0c1ac49bd9bd4c3
ramaanoFullStack <ramaano.m@gmail.com>
2022-09-13 14:19:36 -0700

    adds two intergers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* add - adds rsn1 with rsn2
+*
+* @rsn1: interger 1
+* @rsn2: interger 2
+* Return: rsum
+*/
+int add( int rsn1, int rsn2)
+{
+  int rsum;
+  rsum = rsn1 + rsn2;
+  return (rsum);
+}
