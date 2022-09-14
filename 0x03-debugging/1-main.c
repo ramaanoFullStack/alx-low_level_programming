c66571b5fbdc5501bd719d5abd9d6c241994aac1
ramaanoFullStack <ramaano.m@gmail.com>
2022-09-14 02:52:45 -0700

    Commented out code thats causing an ininate loop

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - infinate loop initiated
+*
+* Return: is 0 (Success)
+*/
+int main(void)
+{
+int i;
+
+printf("Infinite loop incoming :(\n");
+
+i = 0;
+/**
+*{
+* putchar(i);
+*}
+*/
+printf("Infinate loop avoided! \\o/\n");
+
+return (0);
+}
