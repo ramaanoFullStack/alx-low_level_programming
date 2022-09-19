e9160642189ac708857b07eceec9e50eda220a81
ramaanoFullStack <rami.stalin.m@outlook.com>
2022-09-19 10:46:25 -0700

    checks the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _strlen - start program
+* @s: string
+*
+* Return: integer length
+*/
+int_strlen(char *5)
+{
+int length = 0;
+
+while (s[length])
+length++;
+return (length);
+}
