b89c9219476b75b1f9df5916d90ab5395214376d
ramaanoFullStack <rami.stalin.m@outlook.com>
2022-09-19 11:17:16 -0700

    prints a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_rev - start program
+* @s: reference pointer
+* Return: is 0 (Success)
+*/
+void print_rev(char *s)
+{
+int i = 0;
+while (s[i])
+i++;
+while(i--)
+_putchar(s[i]));
+_putchar('\n');
+}
