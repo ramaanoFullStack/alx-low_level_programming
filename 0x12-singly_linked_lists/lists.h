0c5232b22c698a20e8881c4d167805eeb0120ee0
ramaanoFullStack <rami.stalin.m@outlook.com>
2022-10-20 15:47:35 -0700

    Modified lists.h with new prototype

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include<stddef.h>
+#include<stdarg.h>
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 /**
 * Desc: header file containinf prototypes and definitions
@@ -24,6 +29,7 @@ unsigned int len;
 struct list_s *next;
 } list_t;
 
+int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 
