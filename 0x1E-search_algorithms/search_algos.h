fffe8635ff1b93574ab6cbe29e995e47a454cdaa
ramaanoFullStack <rami.stalin.m@outlook.com>
2023-04-13 00:48:33 -0700

    modified search_algos.h with new prototypes

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -39,5 +39,7 @@ typedef struct skiplist_s
 } skiplist_t;
 
 int linear_search(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif /* SEACH_ALGO_H */
