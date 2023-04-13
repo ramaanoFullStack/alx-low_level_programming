5f8cd34c628afafa0738914a5e3f1bf3242dbf54
ramaanoFullStack <rami.stalin.m@outlook.com>
2023-04-13 04:47:18 -0700

    a function that searches for a value in a sorted array of integers using the Jump search algorithm

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -40,6 +40,7 @@ typedef struct skiplist_s
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
 
